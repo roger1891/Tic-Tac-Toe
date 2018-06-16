8b49302febb6151bd7bc49d8aa9075a73266e6e7
roger braun <rogerrainersbraun@gmail.com>
2018-06-16 13:04:31 +0800

    Initial files

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author user
+ */
+public class TicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame ticTacToe = new TicTacToeFrame();
+        ticTacToe.setTitle("Phantom TicTacToe Game!");
+        ticTacToe.setSize(600, 600);
+        ticTacToe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ticTacToe.setLocationRelativeTo(null);
+        ticTacToe.setVisible(true);
+    }
+    
+}
